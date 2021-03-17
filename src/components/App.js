d44ef9d7a0b3eb6fbd79955b37ebfb9f00c38b62
Sergey Soldatov <sasolda3@mts.ru>
2021-03-17 19:52:57 +0300

    fixed pwa

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,42 @@ import React from 'react';
 
 import '../styles/index.scss';
 import Tasks from "./Tasks";
-import {BrowserRouter, Route} from 'react-router-dom';
-import {Redirect, Switch} from 'react-router';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Redirect, Switch } from 'react-router';
 import Header from './common/Header';
 import NotFound from './404';
-import {Box} from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import CheckList from "./CheckList";
 
 function App() {
-  return (
-      <Box display='flex' flexDirection='column' height={1}>
-          <BrowserRouter>
-              <Switch>
-                  <Route path='/tasks'>
-                      <Header/>
-                      <Tasks/>
-                  </Route>
-                  <Route path='/404'>
-                      <NotFound/>
-                  </Route>
-                  <Route path='/checklist'>
-                      <Header/>
-                      <CheckList/>
-                  </Route>
-                  <Route path='*'>
-                      <Redirect to='/404'/>
-                  </Route>
-              </Switch>
-          </BrowserRouter>
-      </Box>
-  );
+    return (
+        <Box display='flex' flexDirection='column' height={1}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/tasks'>
+                        <Header />
+                        <Tasks />
+                    </Route>
+                    <Route path='/404'>
+                        <NotFound />
+                    </Route>
+                    <Route path='/checklist'>
+                        <Header />
+                        <CheckList />
+                    </Route>
+                    <Route path='*'>
+                        <Redirect to='/404' />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </Box>
+    );
 }
 
 export default App;
 
-if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/public/serverWorker.js')
-        .then(reg => console.log('service worker registered'))
-        .catch(err => console.log('service worker not registered', err));
-}
+// if('serviceWorker' in navigator){
+//     navigator.serviceWorker.register('/public/serverWorker.js')
+//         .then(reg => console.log('service worker registered'))
+//         .catch(err => console.log('service worker not registered', err));
+// }
--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Redirect, Switch} from 'react-router'
-import {BrowserRouter, Route} from "react-router-dom";
-import {MuiThemeProvider} from "@material-ui/core";
-import {theme} from "./theme/theme";
+import { Redirect, Switch } from 'react-router'
+import { BrowserRouter, Route } from "react-router-dom";
+import { MuiThemeProvider } from "@material-ui/core";
+import { theme } from "./theme/theme";
 import Login from "./components/Auth/Login";
+import * as serviceWorker from './serviceWorker';
 
-const PrivateRoute = ( props ) => {
-    const user =  window.localStorage.getItem('auth'); // i will fetch token stored in cookie
-    if(user === 'true')
+const PrivateRoute = (props) => {
+    const user = window.localStorage.getItem('auth'); // i will fetch token stored in cookie
+    if (user === 'true')
         return <Route path='/'>
-            <App/>
+            <App />
         </Route>;
     else
-        return <Redirect to='login'/>
+        return <Redirect to='login' />
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-      <MuiThemeProvider theme={theme}>
-          <BrowserRouter>
-              <Switch>
-                  <Route exact path='/login'>
-                      <Login/>
-                  </Route>
-                  <PrivateRoute/>
-              </Switch>
-          </BrowserRouter>
-      </MuiThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <MuiThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/login'>
+                        <Login />
+                    </Route>
+                    <PrivateRoute />
+                </Switch>
+            </BrowserRouter>
+        </MuiThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+serviceWorker.register();
