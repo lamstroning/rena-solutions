c32dad1053384ed967c5b9ebf6c4e1caf95f09b5
lamstroning <lexashob@yandex.ru>
2021-03-23 00:37:48 +0300

    revert

--- src/components/CheckList.js
+++ src/components/CheckList.js
@@ -1,28 +1,38 @@
-import {useEffect} from 'react';
-import {useLocation} from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useLocation } from "react-router-dom";
 import draft from '../asetss/images/draft-icon.png';
 import scrap from '../asetss/images/scrap.png';
 import { makeStyles } from '@material-ui/styles';
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import AddPhotoAlternateIcon from '@material-ui/icons/AddPhotoAlternate';
+import Camera from 'react-html5-camera-photo';
+import 'react-html5-camera-photo/build/css/index.css';
 
 import {
-    Box, Table, TableBody, TableCell, TableHead, TableRow, withStyles, Button, MenuItem
+    Box, Table, TableBody, TableCell, TableHead, TableRow, withStyles, Button, IconButton,
+    Dialog, DialogTitle, DialogContent
 } from '@material-ui/core';
 
 import { theme } from '../theme/theme';
-import {SmallSelect} from '../theme/SmallSelect';
 
 const checkList = [
     {
+        id: 0,
+        action: '',
+        expected: '',
+        result: null
+    },
+    {
         id: 1,
-        action: 'Проверить питание',
-        expected: 'Есть',
-        result: ''
+        action: '',
+        expected: '',
+        result: null
     },
     {
         id: 2,
-        action: 'Замерить напряжение',
-        expected: '220В',
-        result: ''
+        action: '',
+        expected: '',
+        result: null
     }
 ]
 
@@ -34,7 +44,6 @@ const TableHeader = withStyles(theme => ({
         color: 'white'
     }
 }))(Box);
-
 
 const StyledTableHead = withStyles(theme => ({
     root: {
@@ -69,6 +78,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const useStyles2 = makeStyles(theme => ({
+    root: {},
     tableRightBorder: {
         borderWidth: 0,
         borderBottomWidth: 1,
@@ -77,15 +87,51 @@ const useStyles2 = makeStyles(theme => ({
     },
 }));
 
+function SimpleDialog(props) {
+    const { onClose, selectedValue, open } = props;
+
+    const handleClose = () => {
+        onClose(selectedValue);
+    };
+
+    const handleListItemClick = (value) => {
+        onClose(value);
+    };
+
+    return (
+        <Dialog onClose={handleClose} aria-labelledby="simple-dialog-title" open={open}>
+            <DialogTitle id="simple-dialog-title">Прикрепить файл</DialogTitle>
+            <DialogContent dividers alignItems='center'>
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                    <AddAPhotoIcon />
+                </IconButton>
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                    <AddPhotoAlternateIcon />
+                </IconButton>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
 export default function CheckList() {
+    const [open, setOpen] = useState(false);
     const location = useLocation();
 
     const classes = useStyles();
     const classes2 = useStyles2();
+    const classesHeader = useStylesHeder();
 
     useEffect(() => {
         console.log(location.state); // result: 'some_value'
     }, [location]);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+    };
 
     return (
         <Box
@@ -126,26 +172,29 @@ export default function CheckList() {
                         <Box fontSize={24} p={2}>
                             Задание на ремонт №{location.state.number}
                         </Box>
+
                     </Box>
                     <Box display='flex' p={1}>
-                        <Box m={1} >
-                            <img src={scrap} alt='' />
+                        <Box>
+                            <IconButton aria-label="scrap" onClick={handleClickOpen}><img src={scrap} alt='' /></IconButton>
+
                         </Box>
-                        <Box width={140}>
+                        <Box p={1}>
                             <Button
-                                fullWidth
-                                size='large'
+                                size='medium'
                                 variant='contained'
                                 color='primary'
+
+                                style={{ width: '138px', height: '41px' }}
                             >
                                 Заполнен
-                            </Button>
+                        </Button>
                         </Box>
                     </Box>
                 </Box>
                 <Box display='flex' justifyContent='space-between' fontSize={18} p={2}>
                     <Box display='flex' >
-                        <Box width={40}/>
+                        <Box width={40} />
                         <Box fontWeight='bold' p={1} lineHeight={1}>
                             Оборудование
                         </Box>
@@ -166,13 +215,8 @@ export default function CheckList() {
                     mt={2}
                     p={4}
                 >
-                <Box
-                    m={1}
-                    fontSize={24}
-                    fontWeight='bold'
-                    align='center'
-                >
-                    Выполняемые работы
+                    <Box fontWeight="fontWeightBold" fontSize="24px" m={1} align='center'>
+                        Выполняемые работы
                 </Box>
                 </TableHeader>
                 <Box
@@ -183,13 +227,13 @@ export default function CheckList() {
                     <Table stickyHeader>
                         <StyledTableHead>
                             <TableRow>
-                                <TableCell className='border border_bottom border_white'>N/N</TableCell>
-                                <TableCell className='border border_bottom border_white'>Действия</TableCell>
-                                <TableCell className='border border_bottom border_white'>Ожидаемый результат</TableCell>
+                                <TableCell className={classesHeader.tableRightBorder}>N/N</TableCell>
+                                <TableCell className={classesHeader.tableRightBorder}>Действия</TableCell>
+                                <TableCell className={classesHeader.tableRightBorder}>Ожидаемый результат</TableCell>
                                 <TableCell>Фактический результат</TableCell>
                             </TableRow>
                         </StyledTableHead>
-                        <TableBody>
+                        <TableBody >
                             {checkList.map(task =>
                                 <TableRow key={task.id}>
                                     <TableCell className={classes.tableRightBorder}>{task.id}</TableCell>
@@ -200,22 +244,24 @@ export default function CheckList() {
                                         {task.expected}
                                     </TableCell>
                                     <TableCell className={classes2.tableRightBorder}>
-                                        <SmallSelect
-                                            fullWidth
-                                            variant='outlined'
-                                            value={task.result}
-                                        >
-                                            <MenuItem value=''>ччч</MenuItem>
-                                            <MenuItem>true</MenuItem>
-                                            <MenuItem>false</MenuItem>
-                                        </SmallSelect>
+                                        {task.result}
                                     </TableCell>
                                 </TableRow>
                             )}
+                            <TableRow>
+                                <TableCell />
+                                <TableCell>
+                                </TableCell>
+                                <TableCell>
+                                </TableCell>
+                                <TableCell>
+                                </TableCell>
+                            </TableRow>
                         </TableBody>
                     </Table>
                 </Box>
             </Box>
+            <SimpleDialog open={open} onClose={handleClose} />
         </Box >
     );
 }
