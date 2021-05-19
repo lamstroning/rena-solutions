b9e658bc38e5abb8ffa2ab80696c7bc97637379b
lamstroning <lexashob@yandex.ru>
2021-05-20 01:06:24 +0300

    add report

--- src/components/Reports/Reports.js
+++ src/components/Reports/Reports.js
@@ -1,7 +1,52 @@
+import {Button} from '@material-ui/core';
+import {Input} from '../common/Inputs';
+
 export default function Reports() {
     return (
-        <div>
-            xxxxx
+        <div className='page container'>
+            <div className='row row_end row_offset-4'>
+                <Button
+                    variant='contained'
+                    color='primary'
+                    className='button'
+                >
+                    Построить отчёт
+                </Button>
+            </div>
+            <div className='card card_rounded row row_top row_offset-4'>
+                <div className='col col_6'>
+                    <div className='row row_offset-2'>
+                        <div className='col col_12'>
+                            <Input label='Введите название оборудования'/>
+                        </div>
+                    </div>
+                    <div className='row row_offset-2'>
+                        <div className='col col_12'>
+                            <Input label='Введите шифр оборудования'/>
+                        </div>
+                    </div>
+                    <div className='row row_offset-2'>
+                        <div className='col col_12'>
+                            <Input label='Исполнитель'/>
+                        </div>
+                    </div>
+                    <div className='row row_offset-2'>
+                        <div className='col col_6'>
+                            <Input label='от'/>
+                        </div>
+                        <div className='col col_6'>
+                            <Input label='до'/>
+                        </div>
+                    </div>
+                </div>
+                <div className='col col_6'>
+                    <div className='row row_offset-2'>
+                        <div className='col col_12'>
+                            <Input label='Введите чек-лист'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
