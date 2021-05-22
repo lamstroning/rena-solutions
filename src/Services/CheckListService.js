dd97db10d405e079db40345de973b7b514dc31ff
lamstroning <lexashob@yandex.ru>
2021-05-23 01:11:37 +0300

    fix table

--- src/Services/CheckListService.js
+++ src/Services/CheckListService.js
@@ -1,3 +1,68 @@
-export function createCheckList() {
-    console.log('new list');
+const checkListFields1 = [
+    {
+        name: 'Проверить питание мотора',
+        value: true
+    },
+    {
+        name: 'Проверить наличие связи',
+        value: true
+    },
+    {
+        name: 'Проверить напряжеине на выхоже РЩ',
+        value: '195...240В'
+    }
+]
+const checkListFields2 = [
+    {
+        name: 'Проверить питание мотора',
+        value: true
+    },
+    {
+        name: 'Проверить наличие связи',
+        value: true
+    },
+    {
+        name: 'Проверить напряжеине на выхоже РЩ',
+        value: '195...240В'
+    }
+]
+const checkListFields3 = [
+    {
+        name: 'Проверить питание мотора',
+        value: true
+    },
+    {
+        name: 'Проверить наличие связи',
+        value: true
+    },
+    {
+        name: 'Проверить напряжеине на выхоже РЩ',
+        value: '195...240В'
+    }
+]
+
+const checkList = [
+    {
+        name: 'Проверка системы управления',
+        equipments: ['KUKA KR 6-2'],
+        checkListFields: checkListFields1
+    },
+    {
+        name: 'Проверка системы управления',
+        equipments: ['KUKA KR 6-2'],
+        checkListFields: checkListFields2
+    },
+    {
+        name: 'Регулировка натяжения зубчатых',
+        equipments: ['KUKA KR 6-2'],
+        checkListFields: checkListFields3
+    }
+]
+
+export function getCheckList() {
+    return checkList;
+}
+
+export function filterNameCheckList(filterName) {
+    return checkList.filter(({name}) => name.indexOf(filterName) != -1);
 }
--- src/Services/TaskService.js
+++ src/Services/TaskService.js
@@ -1,3 +1,10 @@
+const tasks = {
+    name: 'PlazMax CNC-2060',
+    id: 'PlazMax CNC-2060',
+    equipment: 'PlazMax CNC-2060',
+    authorId: 1
+}
+
 export const emptyTask = {
     name: '',
     code: '',
