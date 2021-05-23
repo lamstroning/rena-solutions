43bd4ca56d54febd8fb4bc60420977450f70186a
Sergey Soldatov <sasolda3@mts.ru>
2021-05-23 20:14:37 +0300

    fixed getReport

--- src/Services/ReportService.js
+++ src/Services/ReportService.js
@@ -1,23 +1,9 @@
 import { getUserByName } from './AuthService'
 import { getActionByID } from './CheckListService'
 
-const emptyReport = [
-    {
-        number: 000000000001,
-        action: {
-            desc: 'Проверить питание мотора',
-            expected: 'Есть',
-            value: 'Есть',
-            conflict: false
-        },
-        workerId: 1,
-        endDate: '',
-    }
-]
-
 const testRequest = {
-    name: 'KUKA KR 6-2',
-    workerName: 'Иванов П.С',
+    name: 'KUKA 5 ARC',
+    workerName: 'Петров И. Г.',
     startDate: '2021-04-06',
     endDate: '2021-06-07',
     code: '123111123553',
@@ -52,27 +38,27 @@ function isConflict(action) {
     var actionData = getActionByID(action.actionID)
     switch (actionData.field) {
         case 'select':
-            return action.expected === action.value
+            return !(actionData.expected === action.value)
         case 'number':
-            return action.expected.min <= value && value < action.expected.max
+            return !(actionData.expected.min <= action.value && action.value < actionData.expected.max)
         default:
-            return false
+            return true
     }
 }
 
 export function getReport(request, tasks) {
     var result = []
-    tasks.array.forEach(task => {
+    tasks.forEach(task => {
         if ((task.code === request.code) &&
             (task.name === request.name) &&
-            (getUserByName(request.workerName).id = task.workerId) &&
+            (getUserByName(request.workerName).id === task.workerId) &&
             isDateBetween(task.endDate, request.startDate, request.endDate)) {
             task.checkList.actions.forEach((action) => {
+                var actionData = getActionByID(action.actionID)
                 var data = {
                     number: task.number,
                     action: {
-                        desc: action.desc,
-                        expected: action.expected,
+                        actionData: actionData,
                         value: action.value,
                         conflict: isConflict(action)
                     },
